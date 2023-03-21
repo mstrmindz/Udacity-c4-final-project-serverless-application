7318a45dc8e9cd60f140d6ba02c7be94fe928a37
Mstrmindz <tunjexi@yahoo.com>
2023-03-21 14:40:52 +0100

    completed the client side of the todo app

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,11 @@
-// Once your application is deployed, copy an API id here so that the frontend can interact with it
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '1devaql53m'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // Create an Auth0 application and copy values from it into this map. For example:
-  domain: 'dev-2eepsfvz5vn6l3ap.uk.auth0.com', // Auth0 domain
-  clientId: 'Qecnb7AcK7h3AWtZrmUstuQbHcmGjjFq', // Auth0 client id
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-2eepsfvz5vn6l3ap.uk.auth0.com',            // Auth0 domain
+  clientId: 'Qecnb7AcK7h3AWtZrmUstuQbHcmGjjFq',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
